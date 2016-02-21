d676a2db52092e38e8cad96d51be15d662310aed
Jeff Cross <middlefloor@gmail.com>
2016-02-20 21:49:33 -0800

    WIP: implement observeQuery for null and scalar values
    Only supports orderByKey for now

--- src/utils/query_observable.spec.ts
+++ src/utils/query_observable.spec.ts
@@ -1,12 +1,32 @@
 import {describe, expect, it} from 'angular2/testing';
-import {QueryObservable} from './query_observable';
+import {Observable} from 'rxjs/Observable';
+
+import {observeQuery} from './query_observable';
 
 describe('QueryObservable', () => {
   it('should return an observable', () => {
-    expect(typeof (new QueryObservable()).subscribe === 'function').toBe(true);
+    expect(observeQuery({})).toBeAnInstanceOf(Observable);
   });
 
-  it('should return instances of QueryObservable from operators', () => {
-    expect(new QueryObservable().map(v => v)).toBeAnInstanceOf(QueryObservable);
+
+  it('should immediately emit a query object if passed a plain JS object with only scalar values', () => {
+    var nextSpy = jasmine.createSpy('next');
+    var completeSpy = jasmine.createSpy('complete');
+    var query = {orderByKey: true};
+    var obs = observeQuery(query);
+    obs.subscribe(nextSpy, null, completeSpy);
+    expect(nextSpy).toHaveBeenCalledWith(query);
+    expect(completeSpy).toHaveBeenCalled();
   });
+
+
+  it('should return null if called with no query', () => {
+    var nextSpy = jasmine.createSpy('next');
+    var completeSpy = jasmine.createSpy('complete');
+    var query:any = null;
+    var obs = observeQuery(query);
+    obs.subscribe(nextSpy, null, completeSpy);
+    expect(nextSpy).toHaveBeenCalledWith(null);
+    expect(completeSpy).toHaveBeenCalled();
+  })
 });
--- src/utils/query_observable.ts
+++ src/utils/query_observable.ts
@@ -1,11 +1,29 @@
 import {Observable} from 'rxjs/Observable';
+import {ScalarObservable} from 'rxjs/observable/ScalarObservable';
 import {Operator} from 'rxjs/Operator';
+import {Observer} from 'rxjs/Observer';
 
-export class QueryObservable<T> extends Observable<T> {
-  lift<T, R>(operator: Operator<T, R>): Observable<R> {
-    const observable = new QueryObservable<R>();
-    observable.source = this;
-    observable.operator = operator;
-    return observable;
+export interface Query {
+  orderByKey?: boolean | Observable<boolean>;
+}
+
+export function observeQuery (query: Query): Observable<Query> {
+  if (!isPresent(query)) {
+    return new ScalarObservable(null);
   }
+
+  if (!hasObservableProperties(query)) {
+    return new ScalarObservable(query);
+  }
+  return Observable.create((observer: Observer<Query>) => {
+  });
+}
+
+function hasObservableProperties(query: Query): boolean {
+  if (query.orderByKey instanceof Observable) return true;
+  return false;
+}
+
+function isPresent(val: any): boolean {
+  return val !== undefined && val !== null;
 }
